76c0001bf65c0f879bb85a6d79f04204406ec26f
Juan Carlo Banayo <johncban@gmail.com>
2019-07-23 03:24:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
 INSERT INTO bears VALUES ("Mr. Chocolate");
+INSERT INTO bears VALUES ("Rowdy");
+INSERT INTO bears VALUES ("Tabitha");
+INSERT INTO bears VALUES ("Sargent Brown");
+INSERT INTO bears VALUES ("Melissa");
+INSERT INTO bears VALUES ("Grinch");
+INSERT INTO bears VALUES ("Wendy");
+INSERT INTO bears VALUES (null);
